0b5572b790a4caf80b197275bf57eeffe9e84435
sahil-9898 <sahiltiwari.9898@gmail.com>
2020-10-11 01:29:57 +0530

    bug fixes

--- color.js
+++ color.js
@@ -16,6 +16,7 @@ easyBtn.addEventListener("click", function(){
 	colors = generateRandomColors(numSquares);
 	pickedColor = pickColor();
 	colorDisplay.textContent = pickedColor;
+	h1.style.background = "steelblue";
 	for (var i=0; i<squares.length; i++){
 		if(colors[i]){
 			squares[i].style.background = colors[i];
@@ -32,6 +33,7 @@ hardBtn.addEventListener("click", function(){
 	colors = generateRandomColors(numSquares);
 	pickedColor = pickColor();
 	colorDisplay.textContent = pickedColor;
+	h1.style.background = "steelblue";
 	for (var i=0; i<squares.length; i++){
 		squares[i].style.background = colors[i];
 		squares[i].style.display = "block";
